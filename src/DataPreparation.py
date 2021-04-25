9c8181e36c61c26abe9e73e9675be2c18f347fd7
AbdullahA93 <69842690+AbdullahA93@users.noreply.github.com>
2021-04-24 22:47:24 -0400

    add preprocess function

--- src/DataPreparation.py
+++ src/DataPreparation.py
@@ -20,3 +20,23 @@ class DataPreparation:
         self.y_train = None
         self.X_test = None
         self.y_test = None
+
+    def preprocess(self, x):
+        """
+
+        :param x:this bbjbjfjknhjh
+        :return:khjhjhjjj
+        """
+        try:
+            stop_words = stopwords.words('english')
+            lemmatizer = WordNetLemmatizer()
+            x = x.lower()
+            x = x.translate(str.maketrans('', '', string.punctuation))
+            x = x.split()
+            x = [word for word in x if word not in stop_words]
+            x = [lemmatizer.lemmatize(word) for word in x]
+            x = str(x).replace(',', ' ').replace("'", "")[1:-1]
+            return x
+        except:
+            print(f'There is an error in {x}')
+            return 'empty'
