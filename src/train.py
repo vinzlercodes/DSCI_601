9cbe85a533a8c11b721fef199add27bf991ecb60
AbdullahA93 <69842690+AbdullahA93@users.noreply.github.com>
2021-04-24 23:18:28 -0400

    add main method to train with all 4 classifiers

--- src/train.py
+++ src/train.py
@@ -3,6 +3,11 @@ import numpy as np
 import pandas as pd
 import time
 from sklearn.metrics import classification_report
+from skmultilearn.problem_transform import ClassifierChain
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier,LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree, svm
 
 def train_and_pickle(model,name,X_train,y_train):
     """
@@ -19,3 +24,17 @@ def train_and_pickle(model,name,X_train,y_train):
     print(classification_report(y_pred,y_train))
     with open('../models/'+name+'.pickle', 'wb') as f:
         pickle.dump(model, f)
+
+if __name__ == "__main__":
+    start = time.time()
+    # Load data
+    X_train = pd.read_csv(r'../Data/Train_Features.csv',index_col=0)
+    y_train = pd.read_csv(r'../Data/Train_Labels.csv',index_col=0)
+    classifiers = [{'classifierName':'RandomForest','classifier':RandomForestClassifier(class_weight='balanced')}
+        , {'classifierName':'MNB','classifier':MultinomialNB()},{'classifierName':'SVM','classifier':svm.SVC()},
+                   {'classifierName':'LR','classifier':LogisticRegression()}]
+    for x in classifiers:
+        print(x['classifierName'],x['classifier'])
+        classifierChain = ClassifierChain(x['classifier'])
+        train_and_pickle(classifierChain,x['classifierName'],X_train,y_train)
+    print((time.time()-start)/60 , 'mins')
