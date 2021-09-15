c720206b69f24c9742a7b2a10279e0e34617d8bb
Vinayak Sengupta <vinayak.sengupta@gmail.com>
2021-09-14 20:59:47 -0400

    Update DataPreparation.py
    Pre-process function

--- MSR 2022/DataPreparation.py
+++ MSR 2022/DataPreparation.py
@@ -24,3 +24,26 @@ class DataPreparation:
         self.y_train = None
         self.X_test = None
         self.y_test = None
+
+    def preprocess(self, x):
+        """
+        This function responsible of preprocess for the Text column starting by removing the stop word
+        and using lemmatization technique from nltk library , also remove punctuation and the comma
+        and quotation marks
+        :param x: x here is  the text column in the dataset , this param uses for applying the preprocess steps
+        :return: this will return the column after been preprocessed  or return empty if there is an error
+        """
+
+        try:
+            stop_words = stopwords.words('english')
+            lemmatizer = WordNetLemmatizer()
+            x = x.lower()
+            x = x.translate(str.maketrans('', '', string.punctuation))
+            x = x.split()
+            x = [word for word in x if word not in stop_words]
+            x = [lemmatizer.lemmatize(word) for word in x]
+            x = str(x).replace(',', ' ').replace("'", "")[1:-1]
+            return x
+        except:
+            print(f'There is an error in {x}')
+            return 'empty'
