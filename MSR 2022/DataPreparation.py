daf97544e4cfd325b5f0f7aaacb8d1fbc850aa72
Vinayak Sengupta <vinayak.sengupta@gmail.com>
2021-09-15 11:24:24 -0400

    Update DataPreparation.py

--- MSR 2022/DataPreparation.py
+++ MSR 2022/DataPreparation.py
@@ -33,9 +33,10 @@ class DataPreparation:
         :param x: x here is  the text column in the dataset , this param uses for applying the preprocess steps
         :return: this will return the column after been preprocessed  or return empty if there is an error
         """
+
         try:
-            lemmatizer = WordNetLemmatizer()
             stop_words = stopwords.words('english')
+            lemmatizer = WordNetLemmatizer()
             x = x.lower()
             x = x.translate(str.maketrans('', '', string.punctuation))
             x = x.split()
@@ -44,7 +45,7 @@ class DataPreparation:
             x = str(x).replace(',', ' ').replace("'", "")[1:-1]
             return x
         except:
-            print(f'There is an error while preprocess in {x}')
+            print(f'There is an error in {x}')
             return 'empty'
 
     def labelBinarizer(self, df):
@@ -77,6 +78,36 @@ class DataPreparation:
         tempdf = pd.DataFrame(tempdf, columns=list(mlb.classes_))
         return tempdf
 
+    def Vectorization(self, df):
+        """
+        This function applied Vectorization using TF-IDF  here for each word cell to achieve
+        a weight importance value to a particular word in the list and
+        that will help with highlighting certain syntax words or indicative words
+        that will help with refactoring label prediction
+        :param df:this is the pandas data frame use it with text column to apply Tfidf Vectorizer
+        :return:thus will return x as Vectorized text
+        """
+        v = TfidfVectorizer(max_features=1000)
+        x = v.fit_transform(df['message'])
+
+        return x, v
+
+    def concatnate(self, df):
+        """
+        This function cleans the 'message' column and then replaces the values with the vectorized text for better
+        class to input correlation
+
+        :param x: vectorized dataframe 'text' column
+        :param df: the cleaned and dataframe in use
+        :return df: preprocessed dataframe with vectorized 'message' column
+        """
+
+        df.dropna(subset=["message"], inplace=True)
+        df = df.drop(columns=['description', 'detection_tool', 'type'])
+        df['message'] = df['message'].apply(self.preprocess)
+
+        return df
+
     def __call__(self):
         """
         The __call__ method used here to turn the instances
@@ -85,16 +116,25 @@ class DataPreparation:
         :return:
         """
         # this line will drop the null values of text column
-        self.dataFrame.dropna(subset=["Text"], inplace=True)
+        self.dataFrame.dropna(subset=["message"], inplace=True)
         # here to drop duplicate values of text column
-        self.dataFrame = self.dataFrame.drop_duplicates(subset=['Text'])
+        self.dataFrame = self.dataFrame.drop_duplicates(subset=['message'])
         # implement labelBinarizer function
-        self.classes = self.labelBinarizer(self.dataFrame['Class'])
+        self.classes = self.labelBinarizer(self.dataFrame['type'])
+
+        self.dataFrame = self.dataFrame.reset_index(drop=True)
+        self.dataFrame = pd.concat([self.dataFrame, self.classes], axis=1)
+        # apply Vectorization tfidf function and concatenate the data
+        # vectorOfFeatures, self.vectorzier = self.Vectorization(self.dataFrame)
+        self.dataFrame = self.concatnate(self.dataFrame)
+        # saved the Preprocessed data
+        self.dataFrame.to_csv("../MSR 2022/Preprocessed.csv")
+
 
 if __name__ == "__main__":
     start = time.time()
     # Load data
-    data = pd.read_csv(r'C:\Users\vinay\Desktop\DSCII 601\Project\DSCI_601\MSR_2022\result_mongo.csv')
+    data = pd.read_csv(r'C:\Users\vinay\Desktop\DSCII 601\MSR 2022\result_mongo.csv')
     dataprep = DataPreparation(data)
     dataprep()
     print('The data is ready')
