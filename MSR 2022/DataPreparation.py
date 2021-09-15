e3f03b36222eccc6edd1fd13aea3b88e29d5ffcc
Vinayak Sengupta <vinayak.sengupta@gmail.com>
2021-09-14 22:45:35 -0400

    Update DataPreparation.py

--- MSR 2022/DataPreparation.py
+++ MSR 2022/DataPreparation.py
@@ -25,59 +25,6 @@ class DataPreparation:
         self.X_test = None
         self.y_test = None
 
-    def preprocess(self, x):
-        """
-        This function responsible of preprocess for the Text column starting by removing the stop word
-        and using lemmatization technique from nltk library , also remove punctuation and the comma
-        and quotation marks
-        :param x: x here is  the text column in the dataset , this param uses for applying the preprocess steps
-        :return: this will return the column after been preprocessed  or return empty if there is an error
-        """
-
-        try:
-            stop_words = stopwords.words('english')
-            lemmatizer = WordNetLemmatizer()
-            x = x.lower()
-            x = x.translate(str.maketrans('', '', string.punctuation))
-            x = x.split()
-            x = [word for word in x if word not in stop_words]
-            x = [lemmatizer.lemmatize(word) for word in x]
-            x = str(x).replace(',', ' ').replace("'", "")[1:-1]
-            return x
-        except:
-            print(f'There is an error in {x}')
-            return 'empty'
-
-    def labelBinarizer(self, df):
-        """
-        The function converts the multilabel problem into binary classification across multiple classes.
-        The dataset classes are converted into unique columns and their presence values are encoded by 0 or 1.
-        :param df: the cleaned and lemmatized dataframe
-        :return tempdf: a further preprocessed dataframe with dummy variables
-        """
-        mlb = MultiLabelBinarizer()
-        # select the 'labels' column for dummy creation
-        tempdf = pd.DataFrame(columns=['labels'])
-        for i in df:
-            temp = []
-            try:
-                # separating the classes by whitespace
-                i = i.replace(' ', '')
-                # separating each class entry using ',' delimeter
-                for j in i.split(','):
-                    if j != '':
-                        temp.append(j.strip())
-            except:
-                pass
-            tempdf = tempdf.append(pd.DataFrame({'labels': [temp]}))
-        # storing the classes for each entry in tuples
-        tempdf.apply(lambda x: tuple(x.values))
-        mlb.fit(tempdf['labels'])
-        # creating the dummy variables for each unique class
-        tempdf = mlb.transform(tempdf['labels'])
-        tempdf = pd.DataFrame(tempdf, columns=list(mlb.classes_))
-        return tempdf
-
     def Vectorization(self, df):
         """
         This function applied Vectorization using TF-IDF  here for each word cell to achieve
