fdfa525178734310d216e3f61fcf2026d701df10
AbdullahA93 <69842690+AbdullahA93@users.noreply.github.com>
2021-10-17 22:00:53 -0400

    add new files

--- MSR 2022/DataPreparation.py
+++ MSR 2022/DataPreparation.py
@@ -8,7 +8,7 @@ import string
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-
+from sklearn.feature_extraction.text import CountVectorizer
 
 class DataPreparation:
 
@@ -33,7 +33,6 @@ class DataPreparation:
         :param x: x here is  the text column in the dataset , this param uses for applying the preprocess steps
         :return: this will return the column after been preprocessed  or return empty if there is an error
         """
-
         try:
             stop_words = stopwords.words('english')
             lemmatizer = WordNetLemmatizer()
@@ -88,25 +87,53 @@ class DataPreparation:
         :return:thus will return x as Vectorized text
         """
         v = TfidfVectorizer(max_features=1000)
-        x = v.fit_transform(df['message'])
+        x = v.fit_transform(df['v1_comment'])
+
+        #v = CountVectorizer(min_df=0, lowercase=False)
+        #v.fit(df['v1_comment'])
+        #x= v.transform(df['v1_comment'])
 
         return x, v
 
-    def concatnate(self, df):
+    def concatnate(self, x, df):
         """
-        This function cleans the 'message' column and then replaces the values with the vectorized text for better
+        This function cleans the 'text' column and then replaces the values with the vectorized text for better
         class to input correlation
-
         :param x: vectorized dataframe 'text' column
         :param df: the cleaned and dataframe in use
-        :return df: preprocessed dataframe with vectorized 'message' column
+        :return df: preprocessed dataframe with vectorized 'text' column
         """
-
-        df.dropna(subset=["message"], inplace=True)
-        df = df.drop(columns=['description', 'detection_tool', 'type'])
-        df['message'] = df['message'].apply(self.preprocess)
+        df.dropna(subset=["v1_comment"], inplace=True)
+        df['v1_comment'] = df['v1_comment'].apply(self.preprocess)
+        df = pd.concat([pd.DataFrame(x.toarray()), df], axis=1)
 
         return df
+
+    def split(self, df):
+        """
+        The preprocessed dataset is taken and split into the testing set and training set, X_train is the features
+        that will be fed into the model and y_train are what it should predict based on those inputs (learning). X_test
+        will be the unseen data input which the model will make predictions on and y_test will be used to compare the
+        results and accuracy of predictions.
+        :param df: the preprocessed and cleaned dataset
+        :return: X-train, Y-Train, X-test, y-Test
+        """
+        columns = list(df.columns)
+        X_train, X_test, y_train, y_test = train_test_split(df[columns[:-10]], df[columns[-10:]], test_size=0.25,
+                                                            random_state=42)
+        X_train = X_train.drop(columns=['v1_comment'])
+        X_test = X_test.drop(columns=['v1_comment'])
+
+        categories = list(y_train.columns)
+        for x in categories:
+            y_train.loc[y_train[x] > 0, x] = 1
+            y_test.loc[y_test[x] > 0, x] = 1
+        X_train.to_csv(r'../MSR 2022/Train_Features.csv')
+        y_train.to_csv(r'../MSR 2022/Train_Labels.csv')
+        X_test.to_csv(r'../MSR 2022/Test_Features.csv')
+        y_test.to_csv(r'../MSR 2022/Test_Labels.csv')
+
+        return X_train, X_test, y_train, y_test
 
     def __call__(self):
         """
@@ -116,25 +143,32 @@ class DataPreparation:
         :return:
         """
         # this line will drop the null values of text column
-        self.dataFrame.dropna(subset=["message"], inplace=True)
+        self.dataFrame.dropna(subset=["v1_comment"], inplace=True)
         # here to drop duplicate values of text column
-        self.dataFrame = self.dataFrame.drop_duplicates(subset=['message'])
+        ## FIXME: this line causes the error
+        #self.dataFrame = self.dataFrame.drop_duplicates(subset=['v1_comment'])
         # implement labelBinarizer function
-        self.classes = self.labelBinarizer(self.dataFrame['type'])
-
-        self.dataFrame = self.dataFrame.reset_index(drop=True)
-        self.dataFrame = pd.concat([self.dataFrame, self.classes], axis=1)
-        # apply Vectorization tfidf function and concatenate the data
-        # vectorOfFeatures, self.vectorzier = self.Vectorization(self.dataFrame)
-        self.dataFrame = self.concatnate(self.dataFrame)
+        self.dataFrame = pd.concat([self.dataFrame, pd.get_dummies(self.dataFrame["refactoring_type"])], axis=1)
+        self.dataFrame = self.dataFrame.groupby(["satd_id", "v1_comment"]).sum().reset_index()
+        self.dataFrame = self.dataFrame.drop_duplicates(subset=['satd_id'])
+        self.dataFrame.dropna(subset=["v1_comment"], inplace=True)
+        self.dataFrame = self.dataFrame.drop(columns=["satd_id"]).reset_index()
+        self.dataFrame = self.dataFrame.drop(columns=["index"])
         # saved the Preprocessed data
-        self.dataFrame.to_csv("../MSR 2022/Preprocessed.csv")
+        self.dataFrame.to_csv("../MSR 2022/Preprocessed22.csv")
+        # apply Vectorization tfidf function and concatenate the data
+        vectorOfFeatures, self.vectorzier = self.Vectorization(self.dataFrame)
+        self.dataFrame = self.concatnate(vectorOfFeatures, self.dataFrame)
+        # split the data set
+        self.X_train, self.X_test, self.y_train, self.y_test = self.split(self.dataFrame)
+
+
 
 
 if __name__ == "__main__":
     start = time.time()
     # Load data
-    data = pd.read_csv(r'C:\Users\vinay\Desktop\DSCII 601\MSR 2022\result_mongo.csv')
+    data = pd.read_csv(r'../MSR 2022/sample_SATD30ktop.csv')
     dataprep = DataPreparation(data)
     dataprep()
     print('The data is ready')
